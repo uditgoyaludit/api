6bdce4247c55cdcb3a7afc67f00a74e450e2f9ea
uditgoyaludit <67572998+uditgoyaludit@users.noreply.github.com>
2025-02-08 21:00:16 +0530

    Create retrieve.js

---
+++ pages/api/retrieve.js
@@ -0,0 +1,16 @@
+import fs from 'fs';
+import path from 'path';
+
+export default function handler(req, res) {
+    const filePath = path.join('/tmp/', 'api.txt');
+
+    let content = '';
+    try {
+        content = fs.readFileSync(filePath, 'utf8');
+    } catch (err) {
+        content = 'No content found.';
+    }
+
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(200).send(content);
+}
