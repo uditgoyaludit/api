839e277ad8cd1a6dcaae3451905bd4d4c2b89916
uditgoyaludit <67572998+uditgoyaludit@users.noreply.github.com>
2025-02-08 21:26:14 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,6 @@ export default function Home() {
     return (
         <div style={{ textAlign: 'center', padding: '20px' }}>
             <h1>Welcome to My API</h1>
-            <p>Choose an action:</p>
-            <a href="/save" style={{ marginRight: '20px' }}>Save Content</a>
-            <a href="/retrieve">Retrieve Content</a>
         </div>
     );
 }
